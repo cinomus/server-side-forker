390f91f279fbd58b7d59030c0d65fe4b749443b5
cinomus <57807571+cinomus@users.noreply.github.com>
2020-10-20 22:09:44 +0300

    first commit

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const WebSocket = require('ws')
-const wss = new WebSocket.Server({ port: 80 })
+const wss = new WebSocket.Server({ port: 8080 })
 wss.on('connection', ws => {
   ws.on('message', message => {
     console.log(`Received message => ${message}`)
